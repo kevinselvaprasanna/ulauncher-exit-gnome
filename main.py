708d19d1f2400bbd7ca14e6cd64cd0ea97b84141
Kevin Selva Prasanna <kevinselvaprasanna@gmail.com>
2018-05-16 18:59:52 +0530

    Modified for Openbox

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ class GnomeSessionExtension(Extension):
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         items = []
-        options = ['logout', 'restart', 'reboot', 'shutdown', 'halt', 'power-off']
+        options = ['logout', 'restart', 'reboot', 'shutdown', 'halt', 'power-off' , 'suspend', 'sleep']
         my_list = event.query.split(" ")
         if len(my_list) == 1:
             items.append(get_logout_item())
             items.append(get_reboot_item())
             items.append(get_shutdown_item())
+            items.append(get_suspend_item())
 
             return RenderResultListAction(items)
         else:
@@ -36,29 +37,37 @@ class KeywordQueryEventListener(EventListener):
                         included.append('reboot')
                     elif option in ['logout']:
                         items.append(get_logout_item())
+                    elif option in ['suspend', 'sleep']:
+                        items.append(get_suspend_item())
 
             return RenderResultListAction(items)
 
 
 def get_logout_item():
-    return ExtensionResultItem(icon='images/gnome-logout.png',
+    return ExtensionResultItem(icon='images/logout.png',
                                name='Logout',
                                description='Logout from session',
-                               on_enter=RunScriptAction("gnome-session-quit --logout --no-prompt", None))
+                               on_enter=RunScriptAction("openbox --exit", None))
 
 
 def get_reboot_item():
-    return ExtensionResultItem(icon='images/gnome-reboot.png',
+    return ExtensionResultItem(icon='images/reboot.png',
                                name='Reboot',
                                description='Reboot computer',
-                               on_enter=RunScriptAction("gnome-session-quit --reboot --force", None))
+                               on_enter=RunScriptAction("systemctl reboot", None))
 
 
 def get_shutdown_item():
-    return ExtensionResultItem(icon='images/gnome-shutdown.png',
+    return ExtensionResultItem(icon='images/shutdown.png',
                                name='Shutdown',
                                description='Power off computer',
-                               on_enter=RunScriptAction("gnome-session-quit --power-off --force", None))
+                               on_enter=RunScriptAction("systemctl poweroff", None))
+
+def get_suspend_item():
+    return ExtensionResultItem(icon='images/suspend.png',
+                               name='Suspend',
+                               description='Suspend computer',
+                               on_enter=RunScriptAction("systemctl suspend", None))
 
 
 if __name__ == '__main__':
